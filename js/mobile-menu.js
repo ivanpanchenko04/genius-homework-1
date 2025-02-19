4998efa242890408ba45612c992f9a7a9ce9f890
ivanpanchenko04 <89913080+ivanpanchenko04@users.noreply.github.com>
2025-02-19 13:43:51 +0200

    fixed mobile menu translating

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,14 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileBtnOpen = document.querySelector('.menu-btn-open');
 const mobileBtnClose = document.querySelector('.menu-btn-close');
+const mobileMenuLogo = document.querySelector('.mobile-menu__logo');
+const mobileMenuItems = document.querySelectorAll('.mobile-menu__item');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 mobileBtnOpen.addEventListener('click', toggleMenu);
 mobileBtnClose.addEventListener('click', toggleMenu);
+mobileMenuLogo.addEventListener('click', toggleMenu);
+mobileMenuItems.forEach(item => {
+  item.addEventListener('click', toggleMenu);
+});
